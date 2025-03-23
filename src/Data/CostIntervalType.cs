faf85ea667a98227ee181402689f86b2a0d30de6
Thomas Roschinsky <roschinsky@hotmail.com>
2025-03-23 22:03:05 +0100

    extracted CostIntervalType enum as separate file

---
+++ src/Data/CostIntervalType.cs
@@ -0,0 +1,15 @@
+﻿namespace MoneyBurned.Dotnet.Lib.Data;
+
+public enum CostIntervalType
+{
+    Minute,
+    Hour,
+    Day,
+    WorkDay,
+    Week,
+    WorkWeek,
+    Month,
+    WorkMonth,
+    Year,
+    WorkYear
+}
